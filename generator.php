ad1a72f6bc99d9520d2a398144a0fcb5eb2a3925
morontt <morontt@gmail.com>
2015-06-25 00:07:47 +0300

    create input file generator

---
+++ generator.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+
+set_time_limit(0);
+
+$f = fopen(__DIR__ . '/input.dat', 'w');
+for ($i = 0; $i < 100; $i++) {
+    $str = sprintf(
+        "%s%s%s\n",
+        mt_rand(1, 9),
+        substr(mt_rand(0, 9999) . mt_rand(0, 9999) . mt_rand(0, 9999) . mt_rand(0, 9999), 0, 4),
+        substr(mt_rand(0, 9999) . mt_rand(0, 9999) . mt_rand(0, 9999) . mt_rand(0, 9999), 0, 4)
+    );
+    fwrite($f, $str);
+}
+fclose($f);

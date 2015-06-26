7b7709521e530b6c636fc9a6a5077fc7466fd510
morontt <morontt@gmail.com>
2015-06-26 10:53:19 +0300

    fix code style

--- golden_section.py
+++ golden_section.py
@@ -2,7 +2,7 @@
 
 import math
 
-fi = (math.sqrt(5.0) + 1.0)/2
+fi = (math.sqrt(5.0) + 1.0) / 2
 
 print 'epsilon: ',
 eps = input()
@@ -13,8 +13,8 @@ a = input()
 print 'b: ',
 b = input()
 
-x1 = b - (b - a)/fi
-x2 = a + (b - a)/fi
+x1 = b - (b - a) / fi
+x2 = a + (b - a) / fi
 
 print ''
 print 'f[{}]: '.format(x1),
@@ -27,17 +27,17 @@ while (b - a) > eps:
     if f1 >= f2:
         a = x1
         x1 = x2
-        x2 = a + (b - a)/fi
+        x2 = a + (b - a) / fi
         f1 = f2
         print 'f[{}]: '.format(x2),
         f2 = input()
     else:
         b = x2
         x2 = x1
-        x1 = b - (b - a)/fi
+        x1 = b - (b - a) / fi
         f2 = f1
         print 'f[{}]: '.format(x1),
         f1 = input()
 
 print ''
-print 'result: ', (a + b)/2
+print 'result: ', (a + b) / 2
